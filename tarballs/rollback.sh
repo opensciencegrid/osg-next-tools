0675b7c8e4a5736abdd60395bec3c7e6f59354fd
Matyas Selmeci <matyas@cs.wisc.edu>
2013-08-30 22:54:40 +0000

    More tweaks to scripts.
    Add default directory to scripts
    Have migrate-tarball run mk-sims.sh to make new symlinks afterward
    Rename verify-tarball-migration to match the name of the script it's verifying
    Hide backup and .old directories (since this is to be run on a public webserver)
    
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-next-tools@17519 4e558342-562e-0410-864c-e07659590f8c

--- tarballs/rollback.sh
+++ tarballs/rollback.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-tarballs_dir=${1%/}
-tarballs_dir_bak=${tarballs_dir}.bak
+tarballs_dir=${1:-/usr/local/repo/tarball-install}
+tarballs_dir=$(dirname "$tarballs_dir")/$(basename "$tarballs_dir")
+tarballs_dir_bak=$(dirname "$tarballs_dir")/.$(basename "$tarballs_dir").bak
 
 die () {
     echo "$@" 1>&2
     exit 1
 }
-
-if [[ -z $tarballs_dir ]]; then
-    die "Tarballs directory not specified or null. This is /usr/local/repo/tarball-install on repo.grid.iu.edu"
-fi
 
 if [[ ! -d $tarballs_dir ]]; then
     die "$tarballs_dir is not a directory"
@@ -21,5 +18,10 @@ if [[ ! -d $tarballs_dir_bak ]]; then
 fi
 
 # Note: rsync is very particular about trailing slashes.
-rsync -av --delete-after $tarballs_dir_bak/ $tarballs_dir
+rsync -aH --delete-after $tarballs_dir_bak/ $tarballs_dir
+if [[ $? -eq 0 ]]; then
+    echo "Rollback from $tarballs_dir_bak OK"
+else
+    echo "Rollback from $tarballs_dir_bak failed somewhere"
+fi
 
