9210f18f9e6e5a4a9d6c78d9b395a2d578204a37
Matyas Selmeci <matyas@cs.wisc.edu>
2013-08-30 22:28:23 +0000

    Tweak verify scripts to be friendlier.
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-next-tools@17518 4e558342-562e-0410-864c-e07659590f8c

--- tarballs/testing/assert.sh
+++ tarballs/testing/assert.sh
@@ -2,6 +2,8 @@
 
 # If given 2 or more arguments, the first argument is the message to print in
 # case of failure. Otherwise the message is the command that's run.
+# ASSERT_FAIL (not local!) is set to 1 on failure so the script can print an
+# Ok if no failures happened.
 assert () {
     local __msg
     if [[ $# -ge 2 ]]; then
@@ -10,6 +12,11 @@ assert () {
     else
         __msg="$@"
     fi
-    eval "$@" || echo "Assert fail: $__msg"
+    eval "$@"
+    if [[ $? -ne 0 ]]; then
+        ASSERT_FAIL=1
+        echo "Assert fail: $__msg"
+        return $?
+    fi
 }
 
